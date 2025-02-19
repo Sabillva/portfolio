eb31b9cd7c43a1ca068a42c5136e881c9a481153
Elyaaa001 <gabibovaelina75@gmail.com>
2025-02-19 12:48:15 +0400

    jsx file updated

--- frontend/login-signup/vite-project/src/App.jsx
+++ frontend/login-signup/vite-project/src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Register from './pages/Register'
 
 function App() {
+
   return (
-    <div>Welcome</div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
--- frontend/login-signup/vite-project/src/pages/Home.jsx
+++ frontend/login-signup/vite-project/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Register = () =>{
     return(
-    <div>Register</div>
+    <div className='text-3xl text-blue-500'>Home</div>
     )
 }
 export default Register
